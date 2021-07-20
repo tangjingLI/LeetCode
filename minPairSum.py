5d619685d5cfbed5f8d1944b063d8b5b07eb72d5
tangjingLI <942855221@qq.com>
2021-07-20 11:31:57 +0800

    minPairSum

---
+++ minPairSum.py
@@ -0,0 +1,20 @@
+# @Author ltj
+# @Time   2021/7/20 11:26
+# @File   minPairSum.py
+
+class Solution(object):
+    def minPairSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        nums.sort()
+        n = len(nums)
+        res = 0
+
+        for i in range(0, n // 2):
+            temp = nums[i] + nums[n - i - 1]
+            res = max(res, temp)
+            
+        return res
